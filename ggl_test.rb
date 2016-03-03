1d01994f142bfaa7939cfc4af5b4d1d5c22ccf4e
Izaac Zavaleta <izavaleta@tripwire.com>
2016-03-03 11:47:32 -0700

    switched from test-unit to capybara/rspec combo

--- ggl_test.rb
+++ ggl_test.rb
@@ -1,35 +1,23 @@
 require 'rubygems'
 require 'selenium-webdriver'
-require 'test/unit'
 require 'capybara/dsl'
-require 'cucumber'
+require 'capybara/rspec'
 
-class CapybaraTestCase < Test::Unit::TestCase
-  include Capybara::DSL
-
-  def teardown
-    Capybara.reset_sessions!
-    Capybara.use_default_driver
-  end
-end
-
-class MyTest < CapybaraTestCase
-
-  # Called before every test method runs. Can be used
-  # to set up fixture information.
-
-  def setup
-    # Do nothing
-    # @selenium = Selenium::WebDriver.for(:firefox)
+describe "the gmail email", :type => :feature do
+  before(:all) do
     Capybara.current_driver = :selenium
     Capybara.app_host = 'https://gmail.com'
     Capybara.run_server = false
   end
 
-  # Called after every test method runs. Can be used to tear
-  # down fixture information.
-  def test_search_ggl
-    visit('/')
+
+  it "should open the gmail login page" do
+    visit "/"
+  end
+
+  after(:each) do
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
   end
 
 end
