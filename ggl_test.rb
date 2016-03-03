3cb6b03435f8ee47c9c1c77fc44f8d313ee55952
Izaac Zavaleta <izavaleta@tripwire.com>
2016-03-03 08:19:26 -0700

    Adding capybara DSL to the test based on test/unit

--- ggl_test.rb
+++ ggl_test.rb
@@ -1,28 +1,35 @@
 require 'rubygems'
 require 'selenium-webdriver'
 require 'test/unit'
-require 'capybara'
+require 'capybara/dsl'
 require 'cucumber'
 
-class MyTest < Test::Unit::TestCase
+class CapybaraTestCase < Test::Unit::TestCase
+  include Capybara::DSL
+
+  def teardown
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
+end
+
+class MyTest < CapybaraTestCase
 
   # Called before every test method runs. Can be used
   # to set up fixture information.
 
   def setup
     # Do nothing
-    @selenium = Selenium::WebDriver.for(:firefox)
+    # @selenium = Selenium::WebDriver.for(:firefox)
+    Capybara.current_driver = :selenium
+    Capybara.app_host = 'https://gmail.com'
+    Capybara.run_server = false
   end
 
   # Called after every test method runs. Can be used to tear
   # down fixture information.
   def test_search_ggl
-    @selenium.get('https://www.google.com')
-  end
-
-  def teardown
-    # Do nothing
-    @selenium.quit
+    visit('/')
   end
 
 end
