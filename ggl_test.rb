134c5f0f1497f09b393cfd8b6d0cea6e6bb984b5
Izaac Zavaleta <izavaleta@tripwire.com>
2016-03-04 08:47:05 -0700

    Valid login to a gmail account

--- ggl_test.rb
+++ ggl_test.rb
@@ -3,6 +3,9 @@ require 'selenium-webdriver'
 require 'capybara/dsl'
 require 'capybara/rspec'
 
+GMAIL_USER = ENV['GMAIL_USER']
+GMAIL_PASSWD = ENV['GMAIL_PASSWD']
+
 describe 'the gmail email', :type => :feature do
 
   before(:all) do
@@ -14,6 +17,21 @@ describe 'the gmail email', :type => :feature do
   it 'should open the gmail login page' do
     visit '/'
     expect(page).to have_title 'Gmail'
+    if page.has_link?('gmail-sign-in')
+      page.click_link('gmail-sign-in')
+    end
+
+    File.write('tmp.html', page.body)
+
+    within('#gaia_loginform') do
+      fill_in 'Email', :with => GMAIL_USER
+      click_on 'next'
+    end
+
+    if find_button('signIn')
+      fill_in 'Passwd', :with => GMAIL_PASSWD
+      click_on 'signIn'
+    end
   end
 
   after(:each) do
